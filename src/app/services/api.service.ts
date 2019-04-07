533b06de18d99016c50ac7f6bb1ec5954285f9b4
Luis Inacio <lrs.inacio@gmail.com>
2019-04-07 15:47:16 +0100

    ApiService generic http verb methods

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {StaticService} from './static.service';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+              private statics: StaticService) { }
+
+  getAllObjects<T>(action: string): Observable<T> {
+    return this.http.get(this.statics.endpoint.mock + '/' + action + '/', {}) as Observable<T> ;
+  }
+
+  getObjectById<T>(action: string, id: T): Observable<T> {
+    return this.http.get(this.statics.endpoint.mock + '/' + action + '/' + id, {}) as Observable<T> ;
+  }
+
+  setNewObject<T>(action: string, req: T): Observable<T> {
+    return this.http.post(this.statics.endpoint.mock + '/' + action + '/', req) as Observable<T> ;
+  }
+
+  updateObject<T>(action: string, id: number, req: T): Observable<T> {
+    return this.http.patch(this.statics.endpoint.mock + '/' + action + '/' + id, req) as Observable<T> ;
+  }
+
+  deleteObject<T>(action: string, id: number): Observable<T> {
+    return this.http.delete(this.statics.endpoint.mock + '/' + action + '/' + id) as Observable<T> ;
+  }
 }
