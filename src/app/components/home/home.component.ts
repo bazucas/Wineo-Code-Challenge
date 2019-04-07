85c7cb4398bc14eebf052236f7106f62ced00a25
Luis Inacio <lrs.inacio@gmail.com>
2019-04-07 14:42:50 +0100

    Home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {StaticService} from '../../services/static.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private readonly Wineo = this.statics.images.logoBig;
+
+  constructor(private statics: StaticService) { }
 
   ngOnInit() {
   }
--- src/app/services/static.service.ts
+++ src/app/services/static.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import * as paths from '../../static/paths.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StaticService {
 
-  constructor() { }
+  root = paths.default;
+  images = paths.default.images;
+  endpoint = paths.default.endpoint;
 }
