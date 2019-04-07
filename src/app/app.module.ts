dffdb64cbd5b64db368d8abedfef2ff7c3f6f191
Luis Inacio <lrs.inacio@gmail.com>
2019-04-07 13:48:35 +0100

    Initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { InsertPageComponent } from './components/insert-page/insert-page.component';
+import { ListPageComponent } from './components/list-page/list-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { DetailPageComponent } from './components/detail-page/detail-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    InsertPageComponent,
+    ListPageComponent,
+    PageNotFoundComponent,
+    DetailPageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/insert-page/insert-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insert-page',
+  templateUrl: './insert-page.component.html',
+  styleUrls: ['./insert-page.component.css']
+})
+export class InsertPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
